87fa93109598424fcd5f19609b41332431911707
Sandy <peacesandy04@gmail.com>
2023-08-17 01:33:39 -0700

    finished the prompt for ussd

---
+++ ussd.js
@@ -0,0 +1,18 @@
+let usersTransaction = prompt(` Welcome! Please select transaction Type
+   1. Transfer
+   2. Airtime (self)
+   3. Airtime (others)
+`)
+ 
+if (usersTransaction == 1) {
+    let options = prompt(` Select Destination Bank.
+     1. UBA
+     2. Fidelity
+     3. Access
+     4. First bank
+     5. More
+    `)
+}  if (usersTransaction ==  1 || 2 || 3 || 4) {
+      prompt(`Enter Amount`)
+}
+
